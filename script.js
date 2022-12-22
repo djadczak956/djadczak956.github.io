7fcb03d9f4b53e6b62fce0b32e42ee57396f686d
Damian J <jadczakdamian@gmail.com>
2022-12-22 09:36:46 -0500

    The card opens and closes on click

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 /* turnPage after 2.5 seconds after the page has loaded */
-setTimeout(turnPage, 2500);
+//setTimeout(turnPage, 2500);
 function turnPage() {
-    document.querySelector('.front').classList.add('rotate-front');
-    document.querySelector('.back').classList.add('rotate-back');
-    document.querySelector('.page').classList.add('rotate-page');
+    if (!document.getElementById('thing').classList.contains('rotate-back')) {
+        document.querySelector('.front').classList.add('rotate-front');
+        document.querySelector('.back').classList.add('rotate-back');
+        document.querySelector('.page').classList.add('rotate-page');
+    } else {
+        document.querySelector('.front').classList.remove('rotate-front');
+        document.querySelector('.back').classList.remove('rotate-back');
+        document.querySelector('.page').classList.remove('rotate-page');
+    }
 }
+
